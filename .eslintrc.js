16f99b3ff47f22b9a541de30366749aea9a8ad2d
Ruan Joppert <ruan.joppert@gmail.com>
2022-03-31 00:03:10 -0300

    style: add eslint and prettier
    adding eslint with standard config and prettier for code format

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'prettier',
+    'standard',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  env: {
+    es6: true,
+    node: true,
+    jest: true
+  },
+  rules: {
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off'
+  }
+}
